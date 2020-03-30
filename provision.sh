27a7af4735f35d8c3063d824210112e853a55e49
ashisbitt <ashisbitt@icloud.com>
2020-03-30 16:57:44 +0100

    4

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+
 sudo apt-get install openjdk-8-jdk openjdk-8-jre -y
 cd /etc
 sudo chmod 777 environment
